ed30308e45e4d3b40d259c10a6cc99ba769520cd
Vladislav <vlad.argus@gmail.com>
2023-06-28 20:47:05 +0300

    Done

---
+++ notes.controller.js
@@ -0,0 +1,61 @@
+const fs = require('fs/promises')
+const path = require('path')
+const chalk = require('chalk')
+
+const notesPath = path.join(__dirname, 'db.json')
+
+async function addNote(title) {
+  const notes = await getNotes()
+  const note = {
+    title,
+    id: Date.now().toString(),
+  }
+  notes.push(note)
+  await saveNotes(notes)
+  console.log(chalk.green('Note was added'))
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: 'utf-8' })
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+}
+
+async function saveNotes(notes) {
+  await fs.writeFile(notesPath, JSON.stringify(notes))
+}
+
+async function printNotes() {
+  const notes = await getNotes()
+
+  console.log(chalk.bgBlue('Here is the list of notes:'))
+  notes.forEach(note => {
+    console.log(chalk.blue(note.id, note.title))
+  })
+}
+
+async function removeNote(id) {
+  const notes = await getNotes()
+
+  const filtered = notes.filter(note => note.id !== id)
+
+  await saveNotes(filtered)
+  console.log(chalk.red(`Note with id="${id}" has been removed.`))
+}
+
+async function editNote(id, title) {
+  const notes = await getNotes()
+
+  notes.map(note => {
+    if (note.id === id) note.title = title
+  })
+
+  await saveNotes(notes)
+  console.log(chalk.green(`Note with id: ${id} has been changed.`))
+}
+
+module.exports = {
+  addNote,
+  getNotes,
+  removeNote,
+  editNote,
+}
---
+++ public/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('click', event => {
+  if (event.target.dataset.type === 'remove') {
+    const id = event.target.dataset.id
+
+    remove(id).then(() => {
+      event.target.closest('li').remove()
+    })
+  }
+
+  if (event.target.dataset.type === 'edit') {
+    const id = event.target.dataset.id
+    const title = prompt(
+      'Введите новое значение',
+      event.target.dataset.title
+    ).trim()
+    if (title) {
+      edit(id, title)
+    }
+  }
+})
+
+async function remove(id) {
+  await fetch(`/${id}`, {
+    method: 'DELETE',
+  })
+}
+
+async function edit(id, title) {
+  await fetch(`/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify({ title }),
+  })
+}
